dc7e28c5763d77f35162d1f8a6ce682364de8796
Adnan Ahmad <priadn41529@gmail.com>
2026-06-16 17:30:19 +0500

    feat: setting up User models and ecommerce setting

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+#
+#
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20, blank=True)
+    address = models.TextField(blank=True)
+    city = models.CharField(max_length=100, blank=True)
+    country = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} Profile"
